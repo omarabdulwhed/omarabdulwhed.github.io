d9aca49dd0fce708fdd516350b67521fe2bc2e83
omarabdulwhed <51930089@students.liu.edu.lb>
2022-09-28 21:07:25 +0300

    Trial 02

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var  Original_Content =`<div>
 <h4 class="title"><a href="">TITLE</a></h4>
 <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
 </div>`
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('http://localhost:3000/server')
   .then(response => response.json())
   .then(json => {
     json.forEach(element => {
         var content = Original_Content;
-        content = content.replace('TITLE','OMAR');
+        content = content.replace(Element.name);
     
     var service = document.createElement('div')
     service.innerHTML = content;
